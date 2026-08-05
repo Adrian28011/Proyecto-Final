c19322bf60069f4d7271fdd08048d55d0edc885d
Adrian <20251117@itla.edu.do>
2026-08-05 00:14:42 -0400

    Estructura base, conexión a SQL Server y modelos iniciales

---
+++ Selenium/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Selenium.Models;
+
+namespace Selenium.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<VideoGame> VideoGames => Set<VideoGame>();
+    public DbSet<User> Users => Set<User>();
+}
